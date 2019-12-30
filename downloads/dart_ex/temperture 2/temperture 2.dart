050e603fbc5dcec6315b1a0390a4379c1d47a108
40823141 <40823141@gm.nfu.edu.tw>
2019-12-30 20:27:31 +0800

    按鈕偵測4

--- downloads/dart_ex/temperture 2/temperture 2.dart
+++ downloads/dart_ex/temperture 2/temperture 2.dart
@@ -1,18 +1,19 @@
-int x = 0;
-num F = 86;
-num C = 30;
+//在此輸入溫度
+List candf = ["20C", "30C", "50F", "40C", "23F"];
 
-void main(){
-  if (x == 0) {
-  num F2 = C*9/5+32;
-			print('F=$F2');
-  }
-  else{
-    if (x==1){
-    num C2 = (F-32)*5/9;
-			print('C=$C2');
-  }
+void main() {
+  for (var i in candf) {
+    if (i[2] == "C") {
+      var o = (i[0] + i[1]);
+      var c = num.parse(o);
+      num f = c * 9 / 5 + 32;
 
+      print("C $c °等於F $f °");}
+    if (i[2] == "F") {
+      var o = (i[0] + i[1]);
+      var f = num.parse(o);
+      num c = (f - 32) * 5 / 9;
+      print("F $f °等於C $c ° ");
+    }
   }
-    print('x=0,1切換(華氏、攝氏)');
 }
